a6fe1424611e7a54fdd06728f471a6fe1c158a17
naimur-234964 <nrsazib234964@gmail.com>
2024-09-12 23:45:29 +0600

    typo fixed

--- calculator/main.js
+++ calculator/main.js
@@ -1,4 +1,4 @@
 console.log("Choose an Option : \n1. Add \n2. Subtract \n3. Multiply \n4. Divide");
 
-var firstNumber = parseInt(prompt("Enter First Number : "))
-var secondNumber = parseInt(prompt("Enter Second Number : "))
+var firstNumber = parseInt(prompt("Enter First Number : "));
+var secondNumber = parseInt(prompt("Enter Second Number : "));
