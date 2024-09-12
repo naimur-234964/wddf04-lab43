7183c5a584611bfd4b1b9db2aaee4227f4e28231
naimur-234964 <nrsazib234964@gmail.com>
2024-09-12 23:45:07 +0600

    number input done

--- calculator/main.js
+++ calculator/main.js
@@ -1,1 +1,4 @@
 console.log("Choose an Option : \n1. Add \n2. Subtract \n3. Multiply \n4. Divide");
+
+var firstNumber = parseInt(prompt("Enter First Number : "))
+var secondNumber = parseInt(prompt("Enter Second Number : "))
