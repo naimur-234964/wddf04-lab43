f7641dd1858dafee8ff5e5e80dc080bfa6b97d89
naimur-234964 <nrsazib234964@gmail.com>
2024-09-12 23:55:51 +0600

    checking input value

--- calculator/main.js
+++ calculator/main.js
@@ -2,7 +2,7 @@ console.log("Choose an Option : \n1. Add \n2. Subtract \n3. Multiply \n4. Divide
 
 var firstNumber = parseInt(prompt("Enter First Number : "));
 var secondNumber = parseInt(prompt("Enter Second Number : "));
-var option  = prompt("Choose an Operation : ");
+var option  = parseInt(prompt("Choose an Operation : "));
 
 
 // Checking for isNan
@@ -11,3 +11,8 @@ var secondNumberCon = (isNaN(secondNumber));
 var optionCon = (isNaN(option));
 
 
+if(firstNumberCon || secondNumber || optionCon){
+    document.write("Sorry! It seems you've input a wrong value");
+}else{
+    console.log('input succesful');
+}
