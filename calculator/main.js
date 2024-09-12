b8ac44ff229670a2f7e9c1ceee3aa96e84ae3762
naimur-234964 <nrsazib234964@gmail.com>
2024-09-13 00:51:59 +0600

    subtract initialized

--- calculator/main.js
+++ calculator/main.js
@@ -2,7 +2,7 @@ console.log("Choose an Option : \n1. Add \n2. Subtract \n3. Multiply \n4. Divide
 
 var firstNumber = parseInt(prompt("Enter First Number : "));
 var secondNumber = parseInt(prompt("Enter Second Number : "));
-var option  = parseInt(prompt("Choose an Operation : "));
+var option = parseInt(prompt("Choose an Operation : "));
 
 
 // Checking for isNan
@@ -12,14 +12,18 @@ var optionCon = (isNaN(option));
 var result;
 
 
-if(firstNumberCon || secondNumberCon || optionCon || option  == 0 || option > 4){
+if (firstNumberCon || secondNumberCon || optionCon || option == 0 || option > 4) {
     document.write("Sorry! It seems you've input a wrong value");
-}else{
+} else {
     switch (option) {
         case 1:
             document.write(`The Sum is ${firstNumber + secondNumber}`);
             break;
-    
+
+        case 2:
+            document.write(`The Subtraction is ${firstNumber - secondNumber}`);
+            break;
+
         default:
             break;
     }
